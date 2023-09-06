34e510062cdf23732b492582c6fd33645435195b
Sabin <sabinonweb7@gmail.com>
2023-09-06 22:02:00 +0545

    Ui preliminaries

--- src/components/BubbleSortVisualizer.tsx
+++ src/components/BubbleSortVisualizer.tsx
@@ -4,95 +4,80 @@ import { motion } from "framer-motion";
 
 const BubbleSortVisualizer = () => {
   const [array, setArray] = useState<number[]>([]);
-  const [sorted, setSorted] = useState(false);
+  const [sorted, setSorted] = useState<bool>(false);
 
   let randomArray: number[] = [];
+  let storeArray: number[] = [];
   for (let i = 0; i < 20; i++) {
     randomArray = Array.from({ length: 20 }, () => Math.random() * 100);
+    console.log(randomArray);
   }
 
-  const handleBubbleSortClick = async () => {
-    const sortedArray: number[] = await bubbleSort(randomArray);
+  storeArray = randomArray;
 
+  const handleSorted = () => {
+    const sortedArray = bubbleSort(randomArray);
     setArray(sortedArray);
     setSorted(true);
-    console.log(sortedArray);
   };
 
-  const handleBubbleUnsortClick = async () => {
-    setArray(randomArray);
+  const handleReset = () => {
+    setArray(storeArray);
     setSorted(false);
   };
 
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: { staggerChildren: 0.5, staggerDirection: -1 },
-    },
-  };
-
-  const items = {
-    hidden: { backgroundColor: "red" },
-    show: {
-      backgroundColor: "blue",
-    },
+  const handleNew = () => {
+    setArray(randomArray);
   };
 
   return (
-    <>
-      {sorted ? (
-        <div className="flex self-center text-xl font-bold">
-          Bubble Sort Visualizer
-        </div>
-      ) : null}
-
-      <motion.div
-        className="flex-1 flex items-end"
-        variants={container}
-        initial="hidden"
-        animate={sorted ? "show" : "hidden"}
-        transition={{ duration: 0.3 }}
-      >
-        {sorted &&
-          array.map((value, index) => (
-            <motion.div
-              variants={items}
-              initial="hidden"
-              animate="show"
-              key={index}
-              className={`m-1`}
-              style={{ height: value * 5, width: value * 1.1 }}
-            ></motion.div>
-          ))}
-
-        {!sorted &&
-          randomArray.map((value, index) => (
-            <div
-              key={index}
-              className={`bg-red-900 m-1`}
-              style={{ height: value * 5, width: value * 1.1 }}
-            ></div>
-          ))}
-      </motion.div>
-
-      {!sorted ? (
-        
-        <div className="flex items-center justify-center">
-          <button onClick={handleBubbleSortClick} className="border-2 p-1">
+    <div>
+      {!sorted && (
+        <>
+          <button
+            className="h-10 w-14 bg-green-500 rounded-xl"
+            onClick={handleNew}
+          >
+            New
+          </button>
+          <button
+            className="h-10 w-14 bg-green-500 rounded-xl m-3"
+            onClick={handleSorted}
+          >
             Sort
           </button>
-        </div>
-      ) : null}
+          <div className="flex flex-row">
+            {randomArray.map((value, index) => (
+              <div
+                key={index}
+                className={`bg-red-900 m-1`}
+                style={{ height: value * 5, width: value * 1.1 }}
+              ></div>
+            ))}
+          </div>
+        </>
+      )}
 
-      {sorted ? (
-        <div className="flex justify-center items-center">
-          <button onClick={handleBubbleUnsortClick} className="border-2 p-1">
-            Unsort
+      {sorted && (
+        <>
+          <button
+            className="h-10 w-14 bg-green-500 rounded-xl m-3"
+            onClick={handleReset}
+          >
+            Reset
           </button>
-        </div>
-      ) : null}
-    </>
+          <div className="flex flex-row">
+            {array.map((value, index) => (
+              <div
+                key={index}
+                className={`bg-blue-900 m-1`}
+                style={{ height: value * 5, width: value * 1.1 }}
+              ></div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
